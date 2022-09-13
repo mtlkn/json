6de08e1bf0d993cd6b0f3e8d91e0493c8c25bc01
Yuri Metelkin <ymetelkin@gmail.com>
2022-09-13 15:04:02 -0400

    Added performance tests

---
+++ bench_test.go
@@ -0,0 +1,30 @@
+package json
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func BenchmarkPrimeNumbers(b *testing.B) {
+	fs, err := os.ReadDir("testdata")
+	if err != nil {
+		b.Error("failed to read testdata")
+		b.Fail()
+		return
+	}
+
+	for _, f := range fs {
+		b.Run(f.Name(), func(b *testing.B) {
+			bs, _ := os.ReadFile("testdata/" + f.Name())
+			for i := 0; i < b.N; i++ {
+				_, err := ParseObject(bytes.NewReader(bs))
+				if err != nil {
+					b.Error(err)
+					b.Fail()
+					return
+				}
+			}
+		})
+	}
+}
